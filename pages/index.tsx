1fda6d826233bc78f453765c3ce1117a07771325
darshan260802 <91478282+darshan260802@users.noreply.github.com>
2022-10-06 13:44:21 +0530

    Installed Starter Packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      
+    <div className="index-container">
+      <h1>Hello</h1>
     </div>
   )
 }
